7fd97ffd3c23c4b74893a0e1e5853e8b8577b5a6
Lachlan Rogers <me@lachlanrogers.com>
2024-04-03 22:32:46 +1100

    based sorts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Goodbye, world!");
+
+    generate_number_dist();
 }
+
+
+
+
+fn generate_number_dist() {
+
+    let xs: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("First element of the array: {}", xs[0]);
+
+}
+
+fn bogo_sort() {
+
+
+}
+fn bubble_sort() {
+
+}
+
+fn insertion_sort() {
+
+}
+
